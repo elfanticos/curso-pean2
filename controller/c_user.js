6321d6fe6239a5fab41afaa17f8d03fb7f09349c
Jhonatan Meza <dart.bekicfox@gmail.com>
2018-01-19 12:51:04 -0500

    commit 19/01/2018

--- controller/c_user.js
+++ controller/c_user.js
@@ -2,7 +2,7 @@
 
 // var bcrypt = require('bcrypt-nodejs');
 var bcrypt = require('bcrypt');
-var r_user = require('../model/m_user');
+var m_user = require('../model/m_user');
 
 function pruebas(req, res) {
     res.status(200).send({
@@ -13,33 +13,46 @@ function pruebas(req, res) {
 function saveUser(req,res) {
 	var params = req.body;
 	var user   = {};
-	console.log(params);
-	res.status(200).send({
-        message: params
-    });
-	//DATOS DE USUARIO
-	user.name    = params.name;
-	user.email   = params.email;
-	user.passwod = params.passwod;
-	user.role    = 'ROLE_USER';
-	user.image   = 'null';
-	if(params.password) {
-		//Encriptar contraseña
-		bcrypt.hash(params.password,null,null, function(err,hash){
-			user.password = hash;
-			if(user.name != null && user.email != null) {
-				//Guardar el usuario
-				r_user.saveUser
-			}else {
-				res.status(200).send({message:'Introduce los datos completos'});
-			}
-		});
+	if(params.password || params.name || params.email) {
+		if(params.name != null && params.email != null && params.password != null) {
+			//DATOS DE USUARIO
+			user.name    = params.name;
+			user.email   = params.email;
+			user.password = params.password;
+			user.role    = 'ROLE_ADMIN';
+			user.image   = null;
+			//GUARDAR USUARIO
+			m_user.saveUser(user, function(data) {
+				res.status(200).send({message: data});
+			});
+			
+		}else {
+			res.status(200).send({message:'Introduce los datos completos'});
+		}
 	}else {
 		res.status(200).send({message:'Introduce la contraseña'});
 	}
 }
 
+function loginUser(req,res) {
+	var params   = req.body;
+	var email    = params.email;
+	var password = params.password;
+	if(email != null && password != null) {
+		m_user.findOne(email,password,function(data) {
+			if(data === undefined) {
+				res.status(404).send({message: 'Email o password incorrecto.'});
+			}else {
+				res.status(200).send('Bienvenido...');
+			}
+		});
+	}else {
+		res.status(200).send({message : 'Los campos no pueden estar vacios.'});
+	}
+}
+
 module.exports = {
     pruebas,
-    saveUser
+	saveUser,
+	loginUser
 };
--- model/m_user.js
+++ model/m_user.js
@@ -1,21 +1,37 @@
 'use strict'
 
 var db = require('../index');
-
 function saveUser(data,response) {
 	var sql = `INSERT 
 	             INTO users
 	                  (name,email,password,role,image)
-	           VALUES ($1,$2,$3,$4,$5)`;
-	db.conection.any(sql)
+			   VALUES ($1,$2,$3,$4,$5)`;
+    sql = db.pgpromise.as.format(sql,[data.name,data.email,data.password,data.role, data.image]);
+	db.conection.query(sql)
 		.then(function(data) {
-			response(data);
+			response('Inserto exitosamente');
+		}).catch(function(err) {
+			console.log(err);
+		});
+}
 
-		}).catch(function(error) {
-
+function findOne(email,password,response) {
+	var sql = `SELECT email,
+					  password 
+				 FROM users 
+				WHERE email = $1
+				  AND password = $2 
+				LIMIT 1`;
+	sql = db.pgpromise.as.format(sql,[email,password]);
+	db.conection.any(sql)
+		.then(data => {
+			response(data[0]);
+		}).catch(err => {
+			console.log(err);
 		});
 }
 
 module.exports = {
-	saveUser 
+	saveUser,
+	findOne
 }
